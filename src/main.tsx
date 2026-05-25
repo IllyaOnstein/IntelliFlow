3b1f1196f3c7c6cf8d760b699c086746929865cc
IllyaOnstein <160089625+IllyaOnstein@users.noreply.github.com>
2026-05-25 15:19:08 +0800

    动态svg修复2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
+// 1. 引入你写在 utils 文件夹里的动画脚本
+import { initDynamicFavicon } from './utils/dynamicFavicon';
+
+// 2. 执行动画函数
+initDynamicFavicon();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
